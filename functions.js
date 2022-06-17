49573aaa64a2777a6975b9e3076bebe823661dad
Kantemir <kantemir.kertiev@hotmail.com>
2022-06-17 18:15:53 -0400

    added functions to populate display with numbers

--- functions.js
+++ functions.js
@@ -19,10 +19,37 @@ function operate (operation, a, b) {
 }
 
 
-let displayValue = 0;
+const displayValue = document.getElementById("screen_text");
+let operation = "";
+
+const zero = document.getElementById("zero")
+zero.addEventListener("click", () => displayValue.innerHTML += "0")
+
+const one = document.getElementById("one")
+one.addEventListener("click", () => displayValue.innerHTML += "1")
+
+const two = document.getElementById("two")
+two.addEventListener("click", () => displayValue.innerHTML += "2")
+
+const three = document.getElementById("three")
+three.addEventListener("click", () => displayValue.innerHTML += "3")
+
+const four = document.getElementById("four")
+four.addEventListener("click", () => displayValue.innerHTML += "4")
+
+const five = document.getElementById("five")
+five.addEventListener("click", () => displayValue.innerHTML += "5")
+
+const six = document.getElementById("six")
+six.addEventListener("click", () => displayValue.innerHTML += "6")
 
 const seven = document.getElementById("seven")
-seven.addEventListener("click", () => {
-    document.getElementById("screen_text").innerHTML = "7";
-    console.log("click")
-})
+seven.addEventListener("click", () => displayValue.innerHTML += "7")
+
+const eight = document.getElementById("eight")
+eight.addEventListener("click", () => displayValue.innerHTML += "8")
+
+const nine = document.getElementById("nine")
+nine.addEventListener("click", () => displayValue.innerHTML += "9")
+
+
