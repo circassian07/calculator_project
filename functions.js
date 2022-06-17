fb46e4849b02aa5d4bd27b8e23c83bcd50018e1d
Kantemir <kantemir.kertiev@hotmail.com>
2022-06-17 17:42:49 -0400

    added IDs to buttons

--- functions.js
+++ functions.js
@@ -5,3 +5,24 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 
 const divide = (a,b) => a / b; 
+
+function operate (operation, a, b) {
+    if (operation === "add") {
+        return add(a,b)
+    } else if (operation === "subtract") {
+        return subtract(a,b)
+    } else if (operation === "multiply") {
+        return multiply(a,b)
+    } else if (operation === "divide") {
+        return divide(a,b)
+    }   
+}
+
+
+let displayValue = 0;
+
+const seven = document.getElementById("seven")
+seven.addEventListener("click", () => {
+    document.getElementById("screen_text").innerHTML = "7";
+    console.log("click")
+})
