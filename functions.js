4682e1c3116341de896a05679691fba755209315
Kantemir <kantemir.kertiev@hotmail.com>
2022-06-17 20:02:45 -0400

    arithmetic operations functional

--- functions.js
+++ functions.js
@@ -8,48 +8,105 @@ const divide = (a,b) => a / b;
 
 function operate (operation, a, b) {
     if (operation === "add") {
-        return add(a,b)
+        displayValue.innerHTML = add(a,b);
     } else if (operation === "subtract") {
-        return subtract(a,b)
+        displayValue.innerHTML = subtract(a,b);
     } else if (operation === "multiply") {
-        return multiply(a,b)
+        displayValue.innerHTML = multiply(a,b);
     } else if (operation === "divide") {
-        return divide(a,b)
+        displayValue.innerHTML = divide(a,b);
     }   
 }
 
 
 const displayValue = document.getElementById("screen_text");
-let operation = "";
+const displayOp = document.getElementById("display_op");
+let arithmetic = "";
+let firstNumber = 0;
+let secondNumber = 0;
+
+// numerical buttons
 
 const zero = document.getElementById("zero")
-zero.addEventListener("click", () => displayValue.innerHTML += "0")
+zero.addEventListener("click", () => displayValue.innerHTML += 0)
 
 const one = document.getElementById("one")
-one.addEventListener("click", () => displayValue.innerHTML += "1")
+one.addEventListener("click", () => displayValue.innerHTML += 1)
 
 const two = document.getElementById("two")
-two.addEventListener("click", () => displayValue.innerHTML += "2")
+two.addEventListener("click", () => displayValue.innerHTML += 2)
 
 const three = document.getElementById("three")
-three.addEventListener("click", () => displayValue.innerHTML += "3")
+three.addEventListener("click", () => displayValue.innerHTML += 3)
 
 const four = document.getElementById("four")
-four.addEventListener("click", () => displayValue.innerHTML += "4")
+four.addEventListener("click", () => displayValue.innerHTML += 4)
 
 const five = document.getElementById("five")
-five.addEventListener("click", () => displayValue.innerHTML += "5")
+five.addEventListener("click", () => displayValue.innerHTML += 5)
 
 const six = document.getElementById("six")
-six.addEventListener("click", () => displayValue.innerHTML += "6")
+six.addEventListener("click", () => displayValue.innerHTML += 6)
 
 const seven = document.getElementById("seven")
-seven.addEventListener("click", () => displayValue.innerHTML += "7")
+seven.addEventListener("click", () => displayValue.innerHTML += 7)
 
 const eight = document.getElementById("eight")
-eight.addEventListener("click", () => displayValue.innerHTML += "8")
+eight.addEventListener("click", () => displayValue.innerHTML += 8)
 
 const nine = document.getElementById("nine")
-nine.addEventListener("click", () => displayValue.innerHTML += "9")
+nine.addEventListener("click", () => displayValue.innerHTML += 9)
 
 
+// operational buttons
+
+const added = document.getElementById("add")
+added.addEventListener("click", () => {
+    firstNumber = parseInt(displayValue.innerHTML);
+    displayValue.innerHTML = "";
+    displayOp.innerHTML = "+";
+    arithmetic="add";
+})
+
+const times = document.getElementById("multiply")
+times.addEventListener("click", () => {
+    firstNumber = parseInt(displayValue.innerHTML);
+    displayValue.innerHTML = "";
+    displayOp.innerHTML = "*";
+    arithmetic="multiply";
+})
+
+const minus = document.getElementById("subtract")
+minus.addEventListener("click", () => {
+    firstNumber = parseInt(displayValue.innerHTML);
+    displayValue.innerHTML = "";
+    displayOp.innerHTML = "-";
+    arithmetic="subtract";
+})
+
+const division = document.getElementById("divide")
+division.addEventListener("click", () => {
+    firstNumber = parseInt(displayValue.innerHTML);
+    displayValue.innerHTML = "";
+    displayOp.innerHTML = "/";
+    arithmetic="divide";
+})
+
+// equals button 
+
+const equals = document.getElementById("equals")
+equals.addEventListener("click", () => {
+    displayOp.innerHTML = "=";
+    secondNumber = parseInt(displayValue.innerHTML)
+    operate(arithmetic, firstNumber, secondNumber)
+})
+
+// clear button 
+const clear = document.getElementById("clear")
+clear.addEventListener("click", () => {
+    displayValue.innerHTML = "";
+    displayOp.innerHTML = "";
+    firstNumber = 0;
+    secondNumber = 0;
+    arithmetic = "";
+})
